ad62eaf0ac47fdbb81b9efde8f81efbf5e5730db
Thomas Haukland <thomas.haukland@gmail.com>
2012-05-17 00:57:26 +0200

    ...

--- jeoquery.js
+++ jeoquery.js
@@ -65,6 +65,31 @@ var jeoquery = (function () {
 				callBack);
 	};
 
+	my.earthquakes = function( north, south, east, west, date, maxRows, minMagnitude, callBack ) {
+		var dateParam = date || new Date();
+		var dateQs = dateParam.getFullYear() + '-' + (dateParam.getMonth() + 1) + '-' + dateParam.getDate();
+			getGeoNames("earthquakesJSON", {
+				"north": north,
+				"south": south,
+				"east": east,
+				"west": west,
+				"date": dateQs,
+				"maxRows": maxRows || 10,
+				"minMagnitude": minMagnitude || 0 },
+				callBack);
+	};
+	my.findNearby = function( latitude, longitude, featureClass, featureCode, radius, style, maxRows, callBack ) {
+			getGeoNames("findNearbyJSON", {
+				"lat": latitude,
+				"lng": longitude,
+				"featureClass": featureClass,
+				"featureCode": featureCode,
+				"radius": radius || 100,
+				"maxRows": maxRows || 10,
+				"style": style || 'MEDIUM' },
+				callBack);
+	};
+
   my.postalCodeLookup = function( postalCode, countryCode, callBack ) {  		
 			getGeoNames("postalCodeLookupJSON", {
 				"postalcode": postalCode,
